e3c9b86d0f60826f0e7bb4d56bda15c91a3f1252
igornfaustino <igor@nfaustino.com>
2022-04-06 10:22:40 -0300

    feat: add template operations to internal client

--- lib/application/service/internal-client.ts
+++ lib/application/service/internal-client.ts
@@ -15,10 +15,22 @@ export type MessageData = {
 
 type Success = Omit<MessageData, 'sentAt'> & {sentAt: Date}
 
+export type TemplateCreated = {
+  id: string
+}
+
+export type TemplateUpdated = {
+  id: string,
+  status: 'approved' | 'negated',
+}
+
 export class COMInternal {
   private readonly provider: string
   private readonly ERROR_QUEUE: string
   private readonly SUCCESS_QUEUE: string
+  private readonly TEMPLATE_CREATED_QUEUE: string
+  private readonly TEMPLATE_UPDATED_QUEUE: string
+
   private readonly connectionString: string
 
   constructor({ environment = 'production', provider = 'servicebus', connectionString }: Params) {
@@ -26,6 +38,8 @@ export class COMInternal {
     this.connectionString = connectionString
     this.ERROR_QUEUE = `${environment}--message-fail`
     this.SUCCESS_QUEUE = `${environment}--message-success`
+    this.TEMPLATE_CREATED_QUEUE = `${environment}--template-created`
+    this.TEMPLATE_UPDATED_QUEUE = `${environment}--template-updated`
   }
 
   public async error(data: MessageData) {
@@ -38,5 +52,17 @@ export class COMInternal {
     const sender = SenderFactory.create(this.provider, this.connectionString)
 
     return await sender.dispatch({ ...data, sentAt: data.sentAt.toISOString() }, this.SUCCESS_QUEUE)
+  }
+
+  public async templateCreated(data: TemplateCreated) {
+    const sender = SenderFactory.create(this.provider, this.connectionString)
+
+    return await sender.dispatch({ ...data }, this.TEMPLATE_CREATED_QUEUE)
+  }
+
+  public async templateUpdated(data: TemplateUpdated) {
+    const sender = SenderFactory.create(this.provider, this.connectionString)
+
+    return await sender.dispatch({ ...data }, this.TEMPLATE_UPDATED_QUEUE)
   }
 }
--- test/domain/service/internal-client.spec.ts
+++ test/domain/service/internal-client.spec.ts
@@ -1,6 +1,5 @@
 import tap from 'tap'
-
-import { COMInternal, MessageData } from '../../../lib/application/service/internal-client'
+import { COMInternal, MessageData, TemplateCreated, TemplateUpdated } from '../../../lib/application/service/internal-client'
 import { FakerMessageSender } from '../../../lib/infra/senders/faker/message'
 
 tap.test('should send a success message', async (t) => {
@@ -78,5 +77,43 @@ tap.test('should can retry flag be falsy when not passed', async (t) => {
 
   t.equal(FakerMessageSender.messages.length, 1)
   t.equal(!!(FakerMessageSender.messages[0] as { retrievable: boolean }).retrievable, false)
+  t.end()
+})
+
+tap.test('should send a template created', async (t) => {
+  t.before(() => FakerMessageSender.cleanMessages())
+  const client = new COMInternal({
+    provider: 'faker',
+    connectionString: 'faker_secret'
+  })
+
+  const message = {
+    id: '2323232'
+  }
+
+  await client.templateCreated(message)
+
+  t.equal(FakerMessageSender.messages.length, 1)
+  t.equal((FakerMessageSender.messages[0] as TemplateCreated).id, message.id)
+
+  t.end()
+})
+
+tap.test('should send a template updated', async (t) => {
+  t.before(() => FakerMessageSender.cleanMessages())
+  const client = new COMInternal({
+    provider: 'faker',
+    connectionString: 'faker_secret'
+  })
+
+  await client.templateUpdated({
+    id: '2323232',
+    status: 'approved'
+  })
+
+  t.equal(FakerMessageSender.messages.length, 1)
+  t.equal((FakerMessageSender.messages[0] as TemplateUpdated).id, '2323232')
+  t.equal((FakerMessageSender.messages[0] as TemplateUpdated).status, 'approved')
+
   t.end()
 })
